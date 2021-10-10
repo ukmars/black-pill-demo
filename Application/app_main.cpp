93044dd90a5e90e4941b7177588681e60ad6fc09
Peter Harrison <peter.harrison@helicron.net>
2021-10-10 15:03:29 +0100

    slow down the serial lines

--- Application/app_main.cpp
+++ Application/app_main.cpp
@@ -7,7 +7,7 @@ int app_main(void) {
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-		HAL_Delay(10);
+		HAL_Delay(100);
 		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 		printf("\033[38;5;206;52mHello\033[0m");
 		printf("\033[1m LD1: \033[0m %d  %d \r\n", HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13),q++);
